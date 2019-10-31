97b24f34a14095e8b296afc8709b5890291f985c
Turb0Yoda <karanbir.ahuja@gmail.com>
2019-10-31 13:29:47 -0700

    Update update_fedora.sh

--- update_fedora.sh
+++ update_fedora.sh
@@ -2,7 +2,7 @@
 echo "hello"
 echo "installing updates"
 echo "-------------------------"
-sudo dnf update
+sudo dnf update -y
 echo "-------------------------"
 
 echo "-------------------------"
@@ -18,9 +18,9 @@ EOF
 
 gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 7568D9BB55FF9E5287D586017AE645C0CF8E292A
 gpg --armor --export 7568D9BB55FF9E5287D586017AE645C0CF8E292A > key.tmp; sudo rpm --import key.tmp; rm -f key.tmp
-sudo dnf install pritunl-client-electron
+sudo dnf install pritunl-client-electron -y
 echo "-------------------------"
 echo "installing caffeine"
 echo "-------------------------"
-sudo dnf install caffeine
+sudo dnf install caffeine -y
 echo "-------------------------"
